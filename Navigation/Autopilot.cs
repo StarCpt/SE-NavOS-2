d9b66e36a807bc798792c6b69a4dd79f8bf0cd9d
StarCpt <86216339+StarCpt@users.noreply.github.com>
2025-12-17 04:34:59 -0800

    disable/remove debug code in autopilot

--- Navigation/Autopilot.cs
+++ Navigation/Autopilot.cs
@@ -130,43 +130,13 @@ namespace IngameScript
             MovePerAxis(localVelocity.X, localDisplacement.X, _shipMass, thrusters[Direction.Right],    thrusters[Direction.Left],    _thrustDirections[(int)Direction.Right],    _thrustDirections[(int)Direction.Left],    localGravity.X);
             MovePerAxis(localVelocity.Y, localDisplacement.Y, _shipMass, thrusters[Direction.Up],       thrusters[Direction.Down],    _thrustDirections[(int)Direction.Up],       _thrustDirections[(int)Direction.Down],    localGravity.Y);
             MovePerAxis(localVelocity.Z, localDisplacement.Z, _shipMass, thrusters[Direction.Backward], thrusters[Direction.Forward], _thrustDirections[(int)Direction.Backward], _thrustDirections[(int)Direction.Forward], localGravity.Z);
-            //return;
 
-            double invShipMass = 1.0 / _shipMass;
-            double accelRight    = invShipMass * _thrustDirections[(int)Direction.Right];
-            double accelLeft     = invShipMass * _thrustDirections[(int)Direction.Left];
-            double accelUp       = invShipMass * _thrustDirections[(int)Direction.Up];
-            double accelDown     = invShipMass * _thrustDirections[(int)Direction.Down];
-            double accelBackward = invShipMass * _thrustDirections[(int)Direction.Backward];
-            double accelForward  = invShipMass * _thrustDirections[(int)Direction.Forward];
-            Vector3D localAccel = new Vector3D // accel toward target
-            {
-                X = localDisplacement.X > 0 ? accelRight    : accelLeft,
-                Y = localDisplacement.Y > 0 ? accelUp       : accelDown,
-                Z = localDisplacement.Z > 0 ? accelBackward : accelForward,
-            };
-            Vector3D localDecel = new Vector3D // accel away from target
-            {
-                X = localDisplacement.X > 0 ? accelLeft    : accelRight,
-                Y = localDisplacement.Y > 0 ? accelDown    : accelUp,
-                Z = localDisplacement.Z > 0 ? accelForward : accelBackward,
-            };
-            
-            Vector3 timeToDecel = new Vector3
-            {
-                X = (float)ComputeTimeToDecel(Math.Sign(localDisplacement.X) * localVelocity.X, Math.Abs(localDisplacement.X), localAccel.X, localDecel.X),
-                Y = (float)ComputeTimeToDecel(Math.Sign(localDisplacement.Y) * localVelocity.Y, Math.Abs(localDisplacement.Y), localAccel.Y, localDecel.Y),
-                Z = (float)ComputeTimeToDecel(Math.Sign(localDisplacement.Z) * localVelocity.Z, Math.Abs(localDisplacement.Z), localAccel.Z, localDecel.Z),
-            };
-
-            Vector3 ticksToDecel = timeToDecel * UPS;
-
-            Program.optionalInfo =
-                $"DisplacementX {localDisplacement.X:0.0000}\n" +
-                $"DisplacementY {localDisplacement.Y:0.0000}\n" +
-                $"DisplacementZ {localDisplacement.Z:0.0000}\n" +
-                $"\n{optionalInfo2}";
-            optionalInfo2.Clear();
+            //Program.optionalInfo =
+            //    $"DisplacementX {localDisplacement.X:0.0000}\n" +
+            //    $"DisplacementY {localDisplacement.Y:0.0000}\n" +
+            //    $"DisplacementZ {localDisplacement.Z:0.0000}\n" +
+            //    $"\n{optionalInfo2}";
+            //optionalInfo2.Clear();
         }
 
         private static void DampenAllDirectionsFast(Dictionary<Direction, List<IMyThrust>> thrusters, Vector3D localVelocity, double shipMass, double[] thrustDirections)
@@ -179,7 +149,7 @@ namespace IngameScript
             SetThrustRatio(thrusters[Direction.Forward],  (float)( localVelocity.Z * shipMass / thrustDirections[(int)Direction.Forward]  * UPS));
         }
 
-        static StringBuilder optionalInfo2 = new StringBuilder();
+        //static StringBuilder optionalInfo2 = new StringBuilder();
 
         // returns: on target
         private static void MovePerAxis(double velocity, double displacement, double shipMass, List<IMyThrust> approachThrusters, List<IMyThrust> stoppingThrusters, double approachThrust, double stoppingThrust, double gravity)
@@ -202,24 +172,24 @@ namespace IngameScript
             double approachAccel = approachThrust / shipMass;
             double stoppingAccel = stoppingThrust / shipMass;
 
-            optionalInfo2.AppendLine($"velocity: {velocity:0.0000}");
-            optionalInfo2.AppendLine($"displacement: {displacement:0.0000}");
-            optionalInfo2.AppendLine($"approachAccel: {approachAccel:0.0000}");
-            optionalInfo2.AppendLine($"stoppingAccel: {stoppingAccel:0.0000}");
-
             double accelMulti;
             double decelInTimeSteps; // start decel in x seconds. debug use only
             bool shouldAccel = ComputeMaxAxisAccel(velocity, displacement, approachAccel + gravity, stoppingAccel - gravity, out accelMulti, out decelInTimeSteps);
 
-            optionalInfo2.AppendLine($"accelMulti: {accelMulti:0.0000}");
-            optionalInfo2.AppendLine($"decelInTicks: {decelInTimeSteps:0.00}");
+            //optionalInfo2.AppendLine($"velocity: {velocity:0.0000}");
+            //optionalInfo2.AppendLine($"displacement: {displacement:0.0000}");
+            //optionalInfo2.AppendLine($"approachAccel: {approachAccel:0.0000}");
+            //optionalInfo2.AppendLine($"stoppingAccel: {stoppingAccel:0.0000}");
+            //optionalInfo2.AppendLine($"accelMulti: {accelMulti:0.0000}");
+            //optionalInfo2.AppendLine($"decelInTicks: {decelInTimeSteps:0.00}");
+            //optionalInfo2.AppendLine($"g: {gravity:0.0000}");
 
             double totalAccelRatio = 0;
             double totalDecelRatio = 0;
             if (shouldAccel)
             {
                 totalAccelRatio += accelMulti;
-                optionalInfo2.AppendLine("accel");
+                //optionalInfo2.AppendLine("accel");
             }
             else if (velocity <= 0)
             {
@@ -228,7 +198,7 @@ namespace IngameScript
 
                 double accelRatio = (-velocity * shipMass / approachThrust) * UPS;
                 totalAccelRatio += accelRatio;
-                optionalInfo2.AppendLine("decel (forced)");
+                //optionalInfo2.AppendLine("decel (forced)");
             }
             else
             {
@@ -244,13 +214,13 @@ namespace IngameScript
                 if (targetDecelTime <= 0)
                 {
                     decelRatio = (velocity * shipMass / stoppingThrust) * UPS;
-                    optionalInfo2.AppendLine("decel (full)");
+                    //optionalInfo2.AppendLine("decel (full)");
                 }
                 else
                 {
                     double targetDecel = velocity / targetDecelTime;
                     decelRatio = targetDecel / stoppingAccel;
-                    optionalInfo2.AppendLine("decel (smooth)");
+                    //optionalInfo2.AppendLine("decel (smooth)");
                 }
 
                 totalDecelRatio += decelRatio;
@@ -269,12 +239,10 @@ namespace IngameScript
             totalAccelRatio -= minRatio;
             totalDecelRatio -= minRatio;
 
-            optionalInfo2.AppendLine($"g: {gravity:0.0000}");
-
             SetThrustRatio(approachThrusters, (float)totalAccelRatio);
             SetThrustRatio(stoppingThrusters, (float)totalDecelRatio);
 
-            optionalInfo2.AppendLine();
+            //optionalInfo2.AppendLine();
         }
 
         public static bool ComputeMaxAxisAccel(double velocity, double displacement, double accel, double decel, out double bestAccelMulti, out double bestDecelInTimeSteps)
