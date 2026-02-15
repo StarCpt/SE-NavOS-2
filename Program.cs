a3fcfdd03e0c48112ffa60b175c4dc8395671d62
StarCpt <86216339+StarCpt@users.noreply.github.com>
2026-02-15 04:24:22 -0800

    v2.15

--- Program.cs
+++ Program.cs
@@ -130,7 +130,7 @@ const int printInterval = 10;
 
         private DateTime bootTime;
         public const string programName = "NavOS";
-        public const string versionStr = "2.15";
+        public const string versionStr = "2.15.1";
 
         public Config config;
 
